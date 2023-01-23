02101f4940a376e2db34096b6e6f0be13a0912e9
karimberdiDekhkonov <karimberdidekhkonov0921@gmail.com>
2023-01-23 17:43:22 +0100

    datahubApp

---
+++ src/test/java/com/datahub/datahub/DataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datahub.datahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
